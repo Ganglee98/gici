4c18a34a7b77b128710db8dc7b629e363b911e49
Ganglee98 <145244695+Ganglee98@users.noreply.github.com>
2024-07-10 17:09:02 +0800

    Add files via upload

---
+++ uavjiahao/bagplay.sh
@@ -0,0 +1,2 @@
+cd /home/ly/STF/data/uavjiahao/
+rosbag play *.bag -s 0 -r 10
---
+++ uavjiahao/convert.sh
@@ -0,0 +1,4 @@
+cd /home/ly/STF/data/convert_gnss/gici-open/tools/ros
+source devel/setup.bash
+rosrun gici_tools gici_files_to_rosbag_post /home/ly/STF/data/convert_gnss/gici-open/tools/ros/src/gici_tools/option/convert_rosbags_post.yaml
+
